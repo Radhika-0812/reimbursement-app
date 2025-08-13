703708d69b728164ab0412a070fd0498c72724f5
KirthikVaishnav <69428343+KirthikVaishnav@users.noreply.github.com>
2025-08-13 23:29:22 +0530

    Delete be directory

--- be/auth-service/auth-service/src/main/java/com/rms/auth_service/dtos/MessageRes.java
+++
@@ -1,8 +0,0 @@
-package com.rms.auth_service.dtos;
-
-import lombok.*;
-
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor
-public class MessageRes {
-    private String message;
-}
--- be/auth-service/auth-service/src/main/java/com/rms/auth_service/security/JwtService.java
+++
@@ -1,7 +0,0 @@
-package com.rms.auth_service.security;
-
-public interface JwtService {
-    String issueAccessToken(String subject);
-    String validateAndGetSubject(String token);
-    long   getAccessTokenTtlSeconds();
-}
