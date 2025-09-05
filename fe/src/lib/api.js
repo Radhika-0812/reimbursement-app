1b5f93ea52fc2a9de7e1a80aa1fd27f96d9e0d3b
Radhika-0812 <radhikas1225125@gmail.com>
2025-09-05 18:18:01 +0530

    api

--- fe/src/lib/api.js
+++ fe/src/lib/api.js
@@ -5,7 +5,7 @@ import axios from "axios";
 const API_URL =
   (typeof import.meta !== "undefined" && import.meta.env?.VITE_API_URL) ||
   (typeof process !== "undefined" && process.env?.REACT_APP_API_URL) ||
-  "http://localhost:8080";
+  "https://reimbursement-app-7wy3.onrender.com";
 
 const USE_COOKIES =
   ((typeof import.meta !== "undefined" && import.meta.env?.VITE_USE_COOKIES) ||
