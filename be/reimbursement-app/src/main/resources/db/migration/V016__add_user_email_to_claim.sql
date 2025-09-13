3b265030c064227e1f2a5efeba56fdbbdb78c614
Radhika-0812 <radhikas1225125@gmail.com>
2025-09-13 17:24:38 +0530

    db

--- be/reimbursement-app/src/main/resources/db/migration/V016__add_user_email_to_claim.sql
+++ be/reimbursement-app/src/main/resources/db/migration/V016__add_user_email_to_claim.sql
@@ -1,2 +1,2 @@
 -- src/main/resources/db/migration/V016__add_user_email_to_claim.sql
-ALTER TABLE claim ADD COLUMN IF NOT EXISTS user_email varchar(320);
+ALTER TABLE claims ADD COLUMN IF NOT EXISTS user_email varchar(320);
